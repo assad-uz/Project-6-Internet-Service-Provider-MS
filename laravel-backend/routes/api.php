4fade71c25586687e97e291da7d9fbf2afd71e4f
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-12-22 10:41:12 +0600

    Update

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -1,8 +1,51 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\BillingController;
+use App\Http\Controllers\PackageController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ConnectionController;
+use App\Http\Controllers\CustomerTypeController;
+use App\Http\Controllers\DistributionBoxController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Vue থেকে ড্যাশবোর্ডের ডাটা পাওয়ার জন্য
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+// API-এর জন্য resource রুট ব্যবহারের নিয়ম কিছুটা আলাদা
+// এখানে 'apiResource' ব্যবহার করা হয় কারণ API-তে 'create' এবং 'edit' (HTML Form) লাগে না।
+
+// Users
+Route::apiResource('users', UserController::class);
+
+// Customer types
+Route::apiResource('customer_types', CustomerTypeController::class);
+
+// Packages
+Route::apiResource('packages', PackageController::class);
+
+// Areas
+Route::apiResource('areas', AreaController::class);
+
+// Distribution Box
+Route::apiResource('distribution_boxes', DistributionBoxController::class);
+
+// Customers
+Route::apiResource('customers', CustomerController::class);
+
+// Connections
+Route::apiResource('connections', ConnectionController::class);
+
+// Billings
+Route::get('billings/{billing}/invoice', [BillingController::class, 'invoice'])->name('billings.invoice');
+Route::apiResource('billings', BillingController::class);
+
+// Payments
+Route::apiResource('payments', PaymentController::class);
+
+// Reports
+Route::get('/reports', [ReportController::class, 'index'])->name('pages.admin.reports.index');
